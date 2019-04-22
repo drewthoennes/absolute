3f170c88896ee8b4a78ee63653e2d3a9dccb8270
cland29 <carl.lee.landskron@gmail.com>
2019-04-22 17:13:49 -0400

    commi

--- src/frontend/components/Interface/index.js
+++ src/frontend/components/Interface/index.js
@@ -19,8 +19,8 @@ export default class Interface extends React.Component {
 
   componentDidMount() {
     log('Interface mounted');
-    // setInterval(this.cooldown.current.startCooldown(3000), 5000);
-    // this.cooldown.current.startCooldown(5000);
+    //setInterval(this.cooldown.current.startCooldown(3000), 5000);
+    //this.cooldown.current.startCooldown(5000);
   }
 
   tick() {
@@ -40,14 +40,18 @@ export default class Interface extends React.Component {
     );
 
     let selected = (
-      <CooldownButton ref={this.cooldown} text="Click me"/>
+      <CooldownButton ref={this.cooldown} text="Click me!"/>
+      
     );
+
+    
 
     return (
       <div id="interface">
         <div className="areas">
           {unlocked}
         </div>
+        {selected}
         {selected}
       </div>
     );
