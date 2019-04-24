e495b31061ca699fd3560bbcf1d1f070e162fb88
drewthoennes <dthoenne@purdue.edu>
2019-04-24 02:59:24 -0400

    Adds register functionality to modal

--- src/frontend/components/LoginModal/index.js
+++ src/frontend/components/LoginModal/index.js
@@ -20,7 +20,9 @@ export default class LoginModal extends React.Component {
 
     this.state = {
       showModal: false,
+      login: true,
       loginError: '',
+      registerError: '',
       styles: {
         modal: {
           content : {
@@ -41,13 +43,13 @@ export default class LoginModal extends React.Component {
         },
         login: {
           fields: {
-            height: '100%',
+            height: '80%',
             display: 'flex',
             flexDirection: 'column',
             justifyContent: 'center',
             marginLeft: '20%',
             marginRight: '20%',
-            paddingBottom: '20%'
+            paddingBottom: '10%'
           },
           title: {
             textAlign: 'center'
@@ -76,6 +78,53 @@ export default class LoginModal extends React.Component {
             padding: '10px',
             color: 'white'
           }
+        },
+        register: {
+          fields: {
+            height: '80%',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            marginLeft: '20%',
+            marginRight: '20%',
+            paddingBottom: '10%'
+          },
+          title: {
+            textAlign: 'center'
+          },
+          email: {
+            padding: '10px',
+            marginBottom: '5px',
+            fontSize: '12px'
+          },
+          username: {
+            padding: '10px',
+            marginBottom: '5px',
+            fontSize: '12px'
+          },
+          password: {
+            padding: '10px',
+            marginTop: '5px',
+            marginBottom: '5px',
+            fontSize: '12px'
+          },
+          error: {
+            color: 'red',
+            margin: '0px',
+            textAlign: 'center',
+            fontSize: '14px'
+          },
+          register: {
+            marginTop: '5px',
+            backgroundColor: '#cccccc',
+            border: '1px solid #b2b2b2',
+            padding: '10px',
+            color: 'white'
+          }
+        },
+        toggle: {
+          margin: '0px',
+          textAlign: 'center'
         }
       }
     };
@@ -86,12 +135,13 @@ export default class LoginModal extends React.Component {
     this.registerEmail = React.createRef();
     this.registerUsername = React.createRef();
     this.registerPassword = React.createRef();
-    this.registerRePassword = React.createRef();
 
     this.handleOpenModal = this.handleOpenModal.bind(this);
     this.handleCloseModal = this.handleCloseModal.bind(this);
     this.handleKeyDownLogin = this.handleKeyDownLogin.bind(this);
+    this.handleKeyDownRegister = this.handleKeyDownRegister.bind(this);
     this.login = this.login.bind(this);
+    this.register = this.register.bind(this);
   }
 
   handleOpenModal() {
@@ -105,6 +155,12 @@ export default class LoginModal extends React.Component {
   handleKeyDownLogin(event) {
     if (event.key === 'Enter') {
       this.login();
+    }
+  }
+
+  handleKeyDownRegister(event) {
+    if (event.key === 'Enter') {
+      this.register();
     }
   }
 
@@ -129,7 +185,7 @@ export default class LoginModal extends React.Component {
         // Get game
         let token = res.data.token;
         let username = res.data.username;
-        
+
         axios.get('/api/game', {
           headers: {
             Authorization: 'Bearer ' + res.data.token
@@ -175,7 +231,22 @@ export default class LoginModal extends React.Component {
     let email = this.registerEmail.current.value;
     let username = this.registerUsername.current.value;
     let password = this.registerPassword.current.value;
-    let repassword = this.registerRePassword.current.value;
+
+    axios.post('/api/auth/register', {
+      email: email,
+      username: username,
+      password: password
+    }).then(res => {
+      if (!res.data) {
+        this.setState({registerError: 'There was a problem servicing the request'});
+      }
+      else if (res.data.error) {
+        this.setState({registerError: res.data.error});
+      }
+      else {
+        this.setState({login: true, registerError: ''});
+      }
+    });
   }
 
   render () {
@@ -183,22 +254,56 @@ export default class LoginModal extends React.Component {
       <p style={this.state.styles.login.error}>{this.state.loginError}</p>
     );
 
+    let registerError = (
+      <p style={this.state.styles.register.error}>{this.state.registerError}</p>
+    );
+
+    let page;
+    if (this.state.login) {
+      page = (
+        <div style={this.state.styles.login.fields}>
+          <h2 style={this.state.styles.login.title}>Log In</h2>
+          <input type="text" placeholder="Username" style={this.state.styles.login.username} ref={this.loginUsername} onKeyDown={this.handleKeyDownLogin}/>
+          <input type="password" placeholder="Password" style={this.state.styles.login.password} ref={this.loginPassword} onKeyDown={this.handleKeyDownLogin}/>
+          {loginError}
+          <button onClick={this.login} style={this.state.styles.login.login}>Log in</button>
+        </div>
+      );
+    }
+    else {
+      page = (
+        <div style={this.state.styles.register.fields}>
+          <h2 style={this.state.styles.register.title}>Register</h2>
+          <input type="text" placeholder="Email" style={this.state.styles.register.email} ref={this.registerEmail} onKeyDown={this.handleKeyDownRegister}/>
+          <input type="text" placeholder="Username" style={this.state.styles.register.username} ref={this.registerUsername} onKeyDown={this.handleKeyDownRegister}/>
+          <input type="password" placeholder="Password" style={this.state.styles.register.password} ref={this.registerPassword} onKeyDown={this.handleKeyDownRegister}/>
+          {registerError}
+          <button onClick={this.register} style={this.state.styles.register.register}>Register</button>
+        </div>
+      );
+    }
+
+    let pageToggle;
+    if (this.state.login) {
+      pageToggle = (
+        <p style={this.state.styles.toggle} onClick={() => this.setState({login: false})}>Don't have an account? Register here.</p>
+      );
+    }
+    else {
+      pageToggle = (
+        <p style={this.state.styles.toggle} onClick={() => this.setState({login: true})}>Already have an account? Log in here.</p>
+      );
+    }
+
     return (
       <div>
         <Modal
           isOpen={this.state.showModal}
           contentLabel="Minimal Modal Example"
           style={this.state.styles.modal}>
-
           <FontAwesomeIcon icon={faTimes} onClick={this.handleCloseModal} style={this.state.styles.close}/>
-
-          <div style={this.state.styles.login.fields}>
-            <h2 style={this.state.styles.login.title}>Log In</h2>
-            <input type="text" placeholder="Username" style={this.state.styles.login.username} ref={this.loginUsername} onKeyDown={this.handleKeyDownLogin}/>
-            <input type="password" placeholder="Password" style={this.state.styles.login.password} ref={this.loginPassword} onKeyDown={this.handleKeyDownLogin}/>
-            {loginError}
-            <button onClick={this.login} style={this.state.styles.login.login}>Log in</button>
-          </div>
+          {page}
+          {pageToggle}
         </Modal>
       </div>
     );
